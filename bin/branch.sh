5f3c679985475599a486904796945840581da8e6
xujiantong@mac <314783087@qq.com>
2023-03-09 16:28:24 +0800

    第一次打包尝试

--- bin/branch.sh
+++ bin/branch.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-# 基于master创建新分支并同步到远程分支
 git checkout main
 git pull origin main
 read -p "请输入你的分支名字:" BranchName
@@ -7,3 +6,8 @@ git push origin HEAD:$BranchName
 git checkout -b $BranchName origin/$BranchName
 
 echo Create New Branch Success: $BranchName
+
+
+# 基于master创建新分支并同步到远程分支
+
+# webpack/0.0.1 第一次打包尝试
