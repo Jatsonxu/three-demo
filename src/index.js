a655d8f415d768ed6a31f6876f175ab2eb1a06b9
xujiantong@mac <314783087@qq.com>
2023-03-09 16:22:04 +0800

    webpack 第一次打包

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import {sum} from "./js/math";
+
+const {priceFormat} = require('./js/format.js')
+
+console.log(sum(20, 20));
+console.log(priceFormat());
---
+++ src/js/format.js
@@ -0,0 +1,8 @@
+// CommonJS模块化导出 Node
+
+const priceFormat = function () {
+    return "$59.88"
+}
+module.exports = {
+    priceFormat
+}
