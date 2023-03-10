c3497de93801ff0cc239d3d07e9624075c8f974d
xujiantong@mac <314783087@qq.com>
2023-03-11 02:48:52 +0800

    掌握 file-loader 处理图片

--- src/js/element.js
+++ src/js/element.js
@@ -1,7 +1,20 @@
 import "../css/style.css"
 import "../css/title.less"
+import "../css/image.css"
+import Kid from "../img/kid.jpeg"
 
 const divEl = document.createElement("div")
 divEl.className = "title"
 divEl.innerHTML = "你好啊,Jetson"
+
+const bgDivEl = document.createElement("div")
+bgDivEl.className = "image-bg"
+
+const imgEl = document.createElement("img")
+imgEl.src = Kid
+
 document.body.appendChild(divEl)
+document.body.appendChild(bgDivEl)
+document.body.appendChild(imgEl)
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -40,14 +40,30 @@ module.exports = {
                     "less-loader",
                 ]
             },
+            // {
+            //     test: /\.(less|css)$/,
+            //     use: [
+            //         "style-loader",
+            //         "css-loader",
+            //         "less-loader",
+            //     ]
+            // },
+            // 瞧一瞧,看一看,官方文档
+            // https://webpack.js.org/guides/asset-modules/#root
             {
-                test: /\.(less|css)$/,
-                use: [
-                    "style-loader",
-                    "css-loader",
-                    "less-loader",
-                ]
-            },
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        // 默认使用 ES6 模块解析,将其关闭,启用 CommonJS模块
+                        // 不配置这个 HTML 文件中的图片路径会错误
+                        esModule: false,
+                        name: "img/[name]_[hash:6].[ext]"
+                    },
+                },
+
+                type: 'javascript/auto'// 不加这个配置,一张图片打包后悔生成两张
+            }
         ]
     }
 }
