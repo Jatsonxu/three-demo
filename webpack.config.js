d91c4c7c5cb8993fa2a01584052795d04b7693b2
xujiantong@mac <314783087@qq.com>
2023-03-12 21:03:20 +0800

    configure webpack plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 const path = require('path')
-
+const {CleanWebpackPlugin} = require("clean-webpack-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const {DefinePlugin} = require("webpack")
+const CopyWebpackPlugin = require("copy-webpack-plugin")
 module.exports = {
     mode: "development",
     devtool: "source-map",
@@ -9,11 +12,33 @@ module.exports = {
     output: {
         // 这里需要绝对路径
         // __dirname:当前文件路径,so config 文件最好放在根目录~
-        path: path.resolve(__dirname, "./build"),
+        path: path.resolve(__dirname, "./dist"),
         // 打包后的文件名
-        filename: "bundle.js"
+        filename: "build.js"
     },
-    module:{
-        rules:[]
-    }
+    module: {
+        rules: []
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: "./public/index.html",
+            title: "webpack+vue"
+        }),
+        new DefinePlugin({
+            BASE_URL: JSON.stringify("./"),
+        }),
+        new CopyWebpackPlugin({
+            from: "public",
+            to: "./",
+            globOptions: {
+                // 需要忽略的文件
+                ignore: [
+                    "**/index.html"
+                ]
+            }
+        })
+
+
+    ]
 }
