d4c73bc874230f5b0f04296e6c75340797b3310e
xujiantong@mac <314783087@qq.com>
2023-03-12 21:14:45 +0800

    项目初始化

--- webpack.config.js
+++ webpack.config.js
@@ -14,29 +14,76 @@ module.exports = {
         // __dirname:当前文件路径,so config 文件最好放在根目录~
         path: path.resolve(__dirname, "./dist"),
         // 打包后的文件名
-        filename: "build.js"
+        filename: "js/build.js"
     },
     module: {
-        rules: []
+        rules: [
+            {
+                test: /\.js$/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    // loader 加载顺序, 从下往上
+                    "style-loader",
+                    "css-loader",
+                    "postcss-loader"
+                ]
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "less-loader",
+                ]
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/,
+                type: "asset",
+                generator: {
+                    filename: "img/[name]_[hash:6][ext]",
+                },
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 120 * 1024,
+                    }
+                }
+            },
+
+            {
+                test: /\.(eot|otf|ttf|woff2?)$/,
+                type: "asset/resource",
+                generator: {
+                    filename: "font/[name]_[hash:6].[ext]"
+                }
+            }
+
+        ]
     },
     plugins: [
         new CleanWebpackPlugin(),
         new HtmlWebpackPlugin({
-            template: "./public/index.html",
+            // template: "./public/index.html",
             title: "webpack+vue"
         }),
         new DefinePlugin({
             BASE_URL: JSON.stringify("./"),
         }),
         new CopyWebpackPlugin({
-            from: "public",
-            to: "./",
-            globOptions: {
-                // 需要忽略的文件
-                ignore: [
-                    "**/index.html"
-                ]
-            }
+            patterns: [
+                {
+                    from: "public", //从哪里复制
+                    to: './',// 复制到那里去
+                    globOptions: {
+                        // 需要忽略哪些文件
+                        ignore: [
+                            "**/index.html"
+                        ]
+                    }
+                }
+            ]
         })
 
 
